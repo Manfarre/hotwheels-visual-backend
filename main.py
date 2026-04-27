4aa2e251db996adc421f59da41945684e84359ab
Manfarre <manfarre@hotmail.com>
2026-04-26 19:33:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.health import router as health_router
 from routes.ebay_auth import router as ebay_auth_router
 from routes.ebay_tools import router as ebay_tools_router
+from routes.match import router as match_router
 
 app = FastAPI(title="Hot Wheels Visual Backend")
 
@@ -18,3 +19,4 @@ app.add_middleware(
 app.include_router(health_router)
 app.include_router(ebay_auth_router)
 app.include_router(ebay_tools_router)
+app.include_router(match_router)
